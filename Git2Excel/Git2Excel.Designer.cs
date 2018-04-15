f2603e39504d83a573cc7abf1cac626c3fb49e6e
Felipe Podolan <felipe_po@poli.ufrj.br>
2018-04-15 17:42:15 -0300

    Initial commit

---
+++ Git2Excel/Git2Excel.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Git2Excel
+{
+    partial class Git2Excel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private System.Windows.Forms.FolderBrowserDialog Navegar;
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Diretorio = new System.Windows.Forms.Button();
+            this.gitinit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Diretorio
+            // 
+            this.Diretorio.Location = new System.Drawing.Point(13, 13);
+            this.Diretorio.Name = "Diretorio";
+            this.Diretorio.Size = new System.Drawing.Size(75, 23);
+            this.Diretorio.TabIndex = 0;
+            this.Diretorio.Text = "Diretório";
+            this.Diretorio.UseVisualStyleBackColor = true;
+            this.Diretorio.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gitinit
+            // 
+            this.gitinit.Location = new System.Drawing.Point(713, 12);
+            this.gitinit.Name = "gitinit";
+            this.gitinit.Size = new System.Drawing.Size(75, 23);
+            this.gitinit.TabIndex = 1;
+            this.gitinit.Text = "Git Init";
+            this.gitinit.UseVisualStyleBackColor = true;
+            this.gitinit.Click += new System.EventHandler(this.gitinit_Click);
+            // 
+            // Git2Excel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gitinit);
+            this.Controls.Add(this.Diretorio);
+            this.Name = "Git2Excel";
+            this.Text = "Git2Excel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Diretorio;
+        private System.Windows.Forms.Button gitinit;
+    }
+}
---
+++ Git2Excel/Git2Excel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Git2Excel
+{
+    public partial class Git2Excel : Form
+    {
+
+        private String path = "";
+
+        public Git2Excel()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Navegar = new System.Windows.Forms.FolderBrowserDialog();
+            if(Navegar.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = Navegar.SelectedPath;
+                MessageBox.Show(path);
+            }
+        }
+
+        private void gitinit_Click(object sender, EventArgs e)
+        {
+            if(path != "")
+            {
+                MessageBox.Show("Iniciando repositório Git2Excel em: " + path);
+                Init init = new Init(path);
+            }
+            else
+            {
+                MessageBox.Show("Diretório vazio");
+            }
+        }
+    }
+}
